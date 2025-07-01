004f21088aadd2b4d753b85a5ee2ac7605512225
diego ibarra <diegoarrabi1@gmail.com>
2025-07-01 17:12:51 -0400

    Initial Commit

---
+++ myScreenSaver/myScreenSaverView.h
@@ -0,0 +1,12 @@
+//
+//  myScreenSaverView.h
+//  myScreenSaver
+//
+//  Created by Diego Ibarra on 7/1/25.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface myScreenSaverView : ScreenSaverView
+
+@end
